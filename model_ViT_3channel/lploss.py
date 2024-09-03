7ab42711441e84c9dbf1bc731408f3b6989d00d8
gems-hw123 <hw123@ic.ac.uk>
2024-09-03 16:37:17 +0100

    model ViT_3channel

---
+++ model_ViT_3channel/lploss.py
@@ -0,0 +1,39 @@
+import torch
+
+class LpLoss(object):
+    def __init__(self, d=2, p=2, size_average=True, reduction=True):
+        super(LpLoss, self).__init__()
+
+        #Dimension and Lp-norm type are postive
+        assert d > 0 and p > 0
+
+        self.d = d
+        self.p = p
+        self.reduction = reduction
+        self.size_average = size_average
+
+    def rel(self, x, y):
+        num_examples = x.size()[0]
+
+        diff_norms = torch.norm(x.reshape(num_examples,-1) - y.reshape(num_examples,-1), self.p, 1)
+        y_norms = torch.norm(y.reshape(num_examples,-1), self.p, 1)
+
+        if self.reduction:
+            if self.size_average:
+                return torch.mean(diff_norms/y_norms)
+            else:
+                return torch.sum(diff_norms/y_norms)
+
+        return diff_norms/y_norms
+
+    def __call__(self, x, y):
+        return self.rel(x, y)
+    
+
+if __name__ == "__main__":
+    x = torch.randn(1, 1, 224, 224, 224)
+    y = torch.randn(1, 1, 224, 224, 224)
+    
+    lploss = LpLoss(d=3)
+    loss = lploss(x, y)
+    print(loss)
